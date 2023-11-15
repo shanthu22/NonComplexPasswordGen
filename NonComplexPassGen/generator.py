8a81cae3443ecd330b3fff006b29fb5923657860
unknown <ssanthujan@yahoo.com>
2023-11-16 01:46:18 +0530

    Special character

--- NonComplexPassGen/generator.py
+++ NonComplexPassGen/generator.py
@@ -5,7 +5,8 @@ def Generator(wordsArray,specialCharArray):
 
     randomNumber1 = random.randint(1,len(wordsArray)-1)
     randomNumber2 = random.randint(1,len(wordsArray)-1)
-    password= (wordsArray[randomNumber1]+ wordsArray[randomNumber2]).capitalize()
+    randomNumber3 = random.randint(1,len(specialCharArray)-1)
+    password= (wordsArray[randomNumber1]+specialCharArray[randomNumber3] + wordsArray[randomNumber2]).capitalize()
     print(password)
     return password
     
