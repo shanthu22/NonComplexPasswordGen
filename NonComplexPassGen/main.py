5f4ef9a2def99764004f36ec630b2df476081b93
unknown <ssanthujan@yahoo.com>
2023-11-21 03:24:10 +0530

    fxi

--- NonComplexPassGen/main.py
+++ NonComplexPassGen/main.py
@@ -1,12 +1,12 @@
 #with open('words.txt', 'r') as file:
 
-from generator import Generator
-from fileReader import FileReader
-file= open('words.txt','r')
+from .generator import Generator
+from .fileReader import FileReader
+#file= open('words.txt','r')
 
 def main():  
-    wordsArray= FileReader('words.txt')
-    specialCharArray= FileReader('specialChar.txt')
+    wordsArray= FileReader('.words.txt')
+    specialCharArray= FileReader('.specialChar.txt')
     #readData = file.read()
     #words= readData.split()
     #print(words)
